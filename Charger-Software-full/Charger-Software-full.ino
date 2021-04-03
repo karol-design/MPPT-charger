007d3b7df4de1172508fd7cdf67e0a48d8e4d540
karol-design <wojslaw.tech@gmail.com>
2021-04-03 15:56:12 +0200

    Mail loop, SetCharging, generateReport update

--- Charger-Software-full/Charger-Software-full.ino
+++ Charger-Software-full/Charger-Software-full.ino
@@ -1,9 +1,12 @@
 /*  Name: MPPT Solar Charger with Bluetooth communication | Main software file
  *  Author: Karol Wojsław
- *  Date: 02/04/2021 (last release)
+ *  Date: 03/04/2021 (last release)
  */
 
 #include <SoftwareSerial.h>
+
+/*Define time intervals for different loops */
+#define timeMain  300000UL //Time between main loop iterations (300000 ms = 5 minutes)
 
 /*Define pin allocations on Arduino board */
 #define BAT_VOLT_PIN    A0
@@ -25,8 +28,6 @@
 #define R8  12000
 #define R9  9000
 
-/*Bluetooth serial port definition */
-SoftwareSerial bluetooth(BT_RX_PIN, BT_TX_PIN); // RX, TX
 
 void setup() {
   //Input-output pin mode definition
@@ -40,37 +41,46 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 
-  //Software serial and bluetooth serial ports configuration (baud rate: 9600)
-  Serial.begin(9600);
-  bluetooth.begin(9600);
-
-  //Initialisation of the LM2576 - 13.7 V output on default
-  SetCharging(1370); //Set the charging voltage to 13.70 Volts
-  delay(5000); //Wait for steady-state on RC filter (connected to PWM output)
-  digitalWrite(LM_ENABLE_PIN, LOW); //Turn on LM2576 switching buck converter
+  digitalWrite(LM_ENABLE_PIN, HIGH); //Turn off LM2576 switching buck converter by default
 }
 
 
 void loop() {
+  unsigned long currentTime, tempTime, timeDifference = 0; //Variables for time management in the main loop
   int BAT_voltage, PV_voltage, PV_current; //Values of measured voltages and currents in mV
-  
-  //Measure the average of n = 20 readings from analog pins.
-  BAT_voltage = measureAverage(BAT_VOLT_PIN, 20, R5, R6);
-  PV_voltage = measureAverage(PV_VOLT_PIN, 20, R3, R4);
-  PV_current = (measureAverage(PV_CURRENT_PIN, 20, 1, 0) - 2500)*10; //No voltage div, hence R1=0 & R2=1, 2.5 V nominal, then 1mV = 10 mA
+  bool Bulk;
 
-  while(BAT_voltage > 1300){ //Bulk charge with MPPT algorithm implemented
-    SetCharging(1470); //Set the charging voltage to 14.7 Volts
+  /* Main loop of the code set to regularly check state of charge of the battery */
+  while(true){
+    currentTime = millis(); //Save current execution time
+    tempTime = currentTime; //Set tempTime to currentTime at the beginning of the loop
+    timeDifference = 0; //Set timeDifference to 0
+
+    /* Check state of charge of the battery and decide if Bulk or Float charging mode should be executed */
+    if(measureAverage(BAT_VOLT_PIN, 20, R5, R6) < 1300){
+      Bulk == 1; //Set Bulk to 1 if the Battery voltage is lower than 13.0V
+    }else{
+      Bulk == 0; //Set Bulk to 0 if the Battery voltage is higher than 13.0V
+    }
+ 
+    /* --------------------------------- */
+    /* Enter bulk charging mode */
+    while(Bulk == 1 && timeDifference < timeMain){
+      //SetCharging(1470); //Set the charging voltage to 14.7 Volts
+      
+      currentTime = millis();
+      timeDifference = currentTime - tempTime;
+    }
+
+    /* --------------------------------- */
+    /* Enter float-storage charging mode */
+    while(Bulk == 0 && timeDifference < timeMain){
+      //SetCharging(1370); //Set the charging voltage to 13.70 Volts
+
+      currentTime = millis();
+      timeDifference = currentTime - tempTime;
+    }
   }
-
-  //Float-storage charge without MPPT
-  SetCharging(1370); //Set the charging voltage to 13.70 Volts
-  delay(5000); //Wait for steady-state on RC filter (connected to PWM output)
-  digitalWrite(LM_ENABLE_PIN, LOW); //Turn on LM2576 switching buck converter
-
-  //Generate and send a report through Bluetooth and Serial Port
-  generateReport(BAT_voltage, PV_voltage, PV_current, 1); //Bluetooth (bt = 1)
-  generateReport(BAT_voltage, PV_voltage, PV_current, 0); //Serial Port (bt = 0)
 }
 
 
@@ -88,55 +98,45 @@ int measureAverage(int input_pin, int n, int V_div_R1, int V_div_R2){
   return Vin_avr_mV; //Return the average of n readings in mV
 }
 
+
 /* -------------------------------------------------------------------------------------------------------------- */
-/* Function that change the charging voltage (LM2576 Vout) to a given value  */
+/* Function that change the charging voltage (LM2576 Vout) to a set value  */
 void SetCharging(int Voltage_mV){
-  //...
+  int V_ref_mV = ((((R8 + R9)*1235)/R8) - ((R9 * Voltage_mV)/R8));
+  int V_ref = map(V_ref_mV, 0, Vin_mV, 0, 1024); //Map value of Reference voltage in mV to 10bit value (0-1024)
+  analogWrite(PWM_PIN, V_ref); //Set reference voltage on PWM pin
 }
 
 
 /* ---------------------------------------------------------------------------------------------- */
-/* Function that send a report on the current status of the charger through bluetooth/serial port */
+/* Function that send a report on the current status of the charger through bluetooth/USB serial port */
 void generateReport(int BAT_voltage, int PV_voltage, int PV_current, bool Bulk, bool bt){
-  if(bt == 1){
-    bluetooth.print("-----------------------------------------------\n");
-    bluetooth.print("MPPT Solar Charger with Bt | Current status: \nDay mode: ");
-    bluetooth.print(PV_voltage > 1300); //Print 1 if PV panel is in the sunlight (PV_V > 13.0V)
-    bluetooth.print(", Day (1) / Night (0)\nCurrent charging mode:");
-    if(Bulk == 1){
-      bluetooth.print("Bulk mode (MPPT algorithm on)\nBattery voltage: ");
-    }else if(Bulk == 0){
-      bluetooth.print("Float mode (MPPT algorithm off)\nBattery voltage: ");
-    }
-    bluetooth.print(BAT_voltage);
-    bluetooth.print(" mV \nPhotovoltaic panel voltage: ");
-    bluetooth.print(PV_voltage);
-    bluetooth.print(" mV \nPhotovoltaic panel current: ");
-    bluetooth.print(PV_current);
-    bluetooth.print(" mA \nPower delivered by PV panel: ");
-    bluetooth.print(PV_current * PV_voltage);
-    bluetooth.print(" mW \nBattery SOC (state of charge) ");
-    bluetooth.print((BAT_voltage - 11820)/13);
-    bluetooth.print(" % of full capacity \n\n");
-  } else if(bt == 0){
-    Serial.print("-----------------------------------------------\n");
-    Serial.print("MPPT Solar Charger with Bt | Current status: \nDay mode: ");
-    Serial.print(PV_voltage > 1300); //Print 1 if PV panel is in the sunlight (PV_V > 13.0V)
-    Serial.print(", Day (1) / Night (0)\nCurrent charging mode:");
-    if(Bulk == 1){
-      Serial.print("Bulk mode (MPPT algorithm on)\nBattery voltage: ");
-    }else if(Bulk == 0){
-      Serial.print("Float mode (MPPT algorithm off)\nBattery voltage: ");
-    }
-    Serial.print(BAT_voltage);
-    Serial.print(" mV \nPhotovoltaic panel voltage: ");
-    Serial.print(PV_voltage);
-    Serial.print(" mV \nPhotovoltaic panel current: ");
-    Serial.print(PV_current);
-    Serial.print(" mA \nPower delivered by PV panel: ");
-    Serial.print(PV_current * PV_voltage);
-    Serial.print(" mW \nBattery SOC (state of charge) ");
-    Serial.print((BAT_voltage - 11820)/13);
-    Serial.print(" % of full capacity \n\n");
+  int RX_PIN = 0, TX_PIN = 1; //Define serial port pins for standard Arduino USB Serial port (by default)
+  if(bt == true){ //Define serial port pins for bluetooth module if bt == true
+    RX_PIN = BT_RX_PIN;
+    TX_PIN = BT_TX_PIN;
   }
+
+  SoftwareSerial serialPort(RX_PIN, TX_PIN); //Define Software serial port
+  serialPort.begin(9600); //Serial port configuration (baud rate: 9600)
+
+  serialPort.print("---------------------------------------------\n");
+  serialPort.print("MPPT Solar Charger with Bt | Current status: \nPV panel status:: ");
+  serialPort.print(PV_voltage > 1300); //Print 1 if PV panel is illuminated by the sunlight (PV_V > 13.0V)
+  serialPort.print(",Illuminated (1) / Shaded/Night (0)\nCurrent charging mode:");
+  if(Bulk == 1){
+    serialPort.print("Bulk mode (MPPT algorithm on)\nBattery voltage: ");
+  }else if(Bulk == 0){
+    serialPort.print("Float mode (MPPT algorithm off)\nBattery voltage: ");
+  }
+  serialPort.print(BAT_voltage);
+  serialPort.print(" mV \nPhotovoltaic panel voltage: ");
+  serialPort.print(PV_voltage);
+  serialPort.print(" mV \nPhotovoltaic panel current: ");
+  serialPort.print(PV_current);
+  serialPort.print(" mA \nPower delivered by PV panel: ");
+  serialPort.print(PV_current * PV_voltage);
+  serialPort.print(" mW \nBattery SOC (state of charge) ");
+  serialPort.print((BAT_voltage - 11820)/13);
+  serialPort.print(" % of full capacity \n\n\n");
 }
